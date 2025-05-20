aa87da90293ca6088e0b83c540ce4c68b6294ce0
danek52 <guskir3565@gmail.com>
2025-05-20 10:53:19 +0300

    init microservice

---
+++ src/core/config.py
@@ -0,0 +1,15 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class DBConfig(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="DATABASE", env_file=".env")
+    database_host: str
+    database_port: int
+    database_username: str
+    database_password: str
+    database_name: str
+    redis_host: str
+    redis_port: int
+
+
+db_config = DBConfig()
