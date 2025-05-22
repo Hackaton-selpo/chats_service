8572a9ed32981983658d6e1d3ad11b7948f2acbf
danek52 <guskir3565@gmail.com>
2025-05-22 16:40:55 +0300

    add grpc token validator module

--- src/routes.py
+++ src/routes.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
+from src.grpc_token_checker.token_validator import get_current_user
 from src.modules.chats.routes import router as chats_router
 from src.modules.messages.routes import router as messages_router
 
@@ -11,3 +12,10 @@ main_router.include_router(messages_router, prefix="/messages", tags=["messages"
 @main_router.get("/ping")
 async def ping():
     return {"ping": "pong"}
+
+
+@main_router.get("/")
+async def root(
+        _=Depends(get_current_user)
+):
+    pass
---
+++ src/shared/depends.py
@@ -0,0 +1,13 @@
+from fastapi import Request, HTTPException, status
+
+
+def get_access_token_from_headers(
+        request: Request,
+) -> str:
+    headers = request.headers
+    if headers.get("Authorization"):
+        return headers.get("Authorization").split(' ')[-1]
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Token required"
+    )
