cd1e6287f772a24c3e45a93bb25f25fde301a989
danek52 <guskir3565@gmail.com>
2025-05-29 16:57:32 +0300

    minor changes

--- test_ws_client.py
+++ test_ws_client.py
@@ -11,7 +11,6 @@ host = f"ws://127.0.0.1:8000/ws/{JWT_TOKEN}"
 host ="""
 wss://yamata-no-orochi.nktkln.com/chats/ws/eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NSIsImVtYWlsIjoiYWxleHJ1aV9ocG93QG1haWwucnUiLCJyb2xlIjoidXNlciIsImV4cCI6MTc0ODY2MzQ5NSwiaWF0IjoxNzQ4NDQ3NDk1LCJ0b2tlbl90eXBlIjoiYWNjZXNzX3Rva2VuIn0.LYaPfZeXLtxjrnff5C8GUYqdijvSVuN8K7htcxwk5Z7Llar-DxaP-F5svVb9E2xUt-ejxvm4ZYPMbEGJYodUd268uCFIUAWIx9GegPto4MZl2dBQyMd3c-bi4TMqnwFNIy1_xvd_W38pKHQha30AiGsxwjXiSXna2Mq-ycYuGa9oNzstJGffzDDSmlIo8oh8uSs2CVYy6DBaB9eW-nL--KCtuioH4aUw98UyNqFdG8YYvfZjovDcNKjZTkxxusd_koWiol3OKzF9mOmf-IRSyskSS64-TADTvHKM4eN6VfcpkyPskQVTdXmvQnnXR-s7VKbbjZP1zmIjOlEpnwzR2w
 """
->>>>>>> 964923d (fix bugs)
 
 async def connect_websocket():
     async with websockets.connect(
